8ce2dcf5d6203aedf571e2f017a5288ff55a8836
Jaehoon Lee <jaehoonlee@Jaehoons-MacBook-Pro.local>
2021-02-17 17:41:30 +0900

    feat: implement the express server and working on the scss mixin

---
+++ src/ts/index.ts
@@ -0,0 +1,5 @@
+const state = {
+    userInfo: ["user-id", "password", "email"],
+    animalInfo: ["user-id", "animal-id", "animal-name", "animal-image","favourites"],
+    post: ["user-id", "animal-id","animal-name", "animal-image", "favourites", "replys"]
+}
