4b5570cbae18abe06c65ab787ed13b4f3bf3bf04
Jaehoon Lee <jaehoonlee@Jaehoons-MacBook-Pro.local>
2021-02-17 14:54:22 +0900

    feat: set up the initial environment setting

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: ['./src/ts/index.ts', './src/sass/style.scss'],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        exclude: /node_modules/
+      }
+    ],
+  },
+  plugins: [
+    // 컴파일 + 번들링 CSS 파일이 저장될 경로와 이름 지정
+    new MiniCssExtractPlugin({ filename: './style.css' }),
+    new HtmlWebpackPlugin({ title: 'Hot Module Replacement', template: './src/index.html', scriptLoading: 'defer' }),
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'source-map',
+  mode: 'development',
+  devServer: {
+    compress: true,
+    port: 9000,
+  },
+};
