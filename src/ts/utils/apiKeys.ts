0b3f409b497e99fd4f49c0aa3823fc87b4800be9
Jaehoon Lee <jaehoonlee@Jaehoons-MacBook-Pro.local>
2021-02-19 11:50:34 +0900

    feat: reset the previous commit and redo the scss and html refactoring

---
+++ src/ts/utils/apiKeys.ts
@@ -0,0 +1,9 @@
+export const firebaseCon = {
+    apiKey: "AIzaSyBUS6uaZMNJAR-1Nh8Rl29-tRuwcYWe9C4",
+    authDomain: "animal-memorial.firebaseapp.com",
+    projectId: "animal-memorial",
+    storageBucket: "animal-memorial.appspot.com",
+    messagingSenderId: "635450115815",
+    appId: "1:635450115815:web:80355cb81bdc888a6c8442",
+    measurementId: "G-GL9347SSYE"
+}
