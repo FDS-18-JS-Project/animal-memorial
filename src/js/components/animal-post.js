a721ec30048a35d2f06de2272fef768e11dadc68
Bernese-Corgi <jnyng0621@gmail.com>
2021-02-23 13:27:49 +0900

    docs: Modify markup and javascript code position.

--- src/js/components/animal-post.js
+++ src/js/components/animal-post.js
@@ -7,6 +7,25 @@ const countComment = () => {
   $commentCount.textContent = $commentList.childElementCount;
 };
 
+const pastCommentDate = (writtenDate = new Date()) => {
+  const nowDate = new Date();
+  const pastTime = nowDate.getTime() - writtenDate.getTime();
+  const pastMinute = Math.floor(pastTime / 1000 / 60);
+  const pastHour = Math.floor(pastTime / 1000 / 60 / 60);
+  const pastDate = Math.floor(pastTime / 1000 / 60 / 60 / 24);
+  const pastWeek = Math.floor(pastTime / 1000 / 60 / 60 / 24 / 7);
+  const pastMonth = Math.floor(pastTime / 1000 / 60 / 60 / 24 / 30);
+  const pastYear = Math.floor(pastTime / 1000 / 60 / 60 / 24 / 365);
+
+  if (pastMinute < 1) return '• 방금 전';
+  if (pastMinute < 60) return `• ${pastMinute}분 전`;
+  if (pastHour < 24) return `• ${pastHour}시간 전`;
+  if (pastDate < 7) return `• ${pastDate}일 전`;
+  if (pastWeek < 5) return `• ${pastWeek}주 전`;
+  if (pastMonth < 12) return `• ${pastMonth}달 전`;
+  if (pastYear >= 1) return `• ${pastYear}년 전`;
+};
+
 const addNewComment = () => {
   $commentBtn.addEventListener('click', () => {
     if (!$commentInput.value) return;
@@ -26,44 +45,14 @@ const addNewComment = () => {
     $commentRow.appendChild($commentDate);
     $commentRow.appendChild($commentContent);
 
+    const writtenDate = new Date();
+    $commentDate.textContent = `${pastCommentDate(writtenDate)}`;
+
     $commentContent.textContent = $commentInput.value;
     $commentInput.value = '';
 
-    $commentDate.textContent = `${pastCommentDate()}`;
-
     countComment();
-
   });
 };
 
 addNewComment();
-
-const pastCommentDate = () => {
-  const nowDate = new Date();
-  const writtenDate = new Date(2020, 0, 3, 3, 35, 00, 0);
-  const pastTime = (nowDate.getTime() - writtenDate.getTime());
-  const pastMinute = Math.floor(pastTime / 1000 / 60);
-  // const pastMinute = Math.floor(nowDate.getMinutes() - writtenDate.getMinutes());
-  const pastHour = Math.floor(pastTime / 1000 / 60 / 60);
-  const pastDate = Math.floor(pastTime / 1000 / 60 / 60 / 24);
-  const pastWeek = Math.floor(pastTime / 1000 / 60 / 60 / 24 / 7);
-  const pastMonth = Math.floor(pastTime / 1000 / 60 / 60 / 24 / 30);
-  const pastYear = Math.floor(pastTime / 1000 / 60 / 60 / 24 / 365);
-
-  // console.log(nowDate.getMinutes());
-
-  if (pastMinute < 1) return '• 방금 전';
-  if (pastMinute < 60) return `• ${pastMinute}분 전`;
-  if (pastHour < 24) return `• ${pastHour}시간 전`;
-  if (pastDate < 7) return `• ${pastDate}일 전`;
-  if (pastWeek < 5) return `• ${pastWeek}주 전`;
-  if (pastMonth < 30) return `• ${pastMonth}달 전`;
-  if (pastYear >= 13)`• ${pastYear}년 전`;
-  console.log(pastMinute);
-  console.log(pastHour);
-  // if (pastMinute < 60)`• ${pastMinute}분 전`;
-  // if (pastHour < 24)`• ${pastHour}시간 전`;
-  // console.log(pastMinute);
-};
-
-pastCommentDate()
